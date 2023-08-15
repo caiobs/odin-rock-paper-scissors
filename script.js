87d328ff214a0954eba87b006844789152ad8baf
Caio Brandão <caiobrandaos@hotmail.com>
2023-08-15 17:08:39 -0300

    Start javascript Start coding the game in the .js file

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Rock Paper Scissors!')
+function getComputerChoice() {
+   let randomChoice = Math.random()
+   if (randomChoice <= 0.33) {
+    return 'rock'
+   } else if (randomChoice >= 0.67) {
+    return 'paper'
+   } else {
+    return 'scissors'
+   }
+}
+
+let playerSelection = prompt('Rock, paper or scissors?')
+let computerSelection = getComputerChoice ()
+
+function RockPaperScissors(playerSelection, computerSelection) {
+
+    let stringLoser = `You Lose! ${computerSelection} beats ${playerSelection}`
+    let stringWinner = `You Win! ${playerSelection} beats ${computerSelection}`
+
+}
